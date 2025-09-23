6bbe34302461a742fcfad57c5029a0df1a712fc2
rodukao <rodukao@gmail.com>
2025-09-23 11:49:11 -0300

    feat: adiciona validação de email usando a dependência email-validator

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 # IMPORTS
-
 # Banco de dados sqlite
 import sqlite3
+
+# Email validator
+from email_validator import validate_email, EmailNotValidError
 
 # CRIA TABELA SE NÃO EXISTIR
 def cria_tabela_usuarios():
@@ -25,17 +27,37 @@ def cria_tabela_usuarios():
 
 # INSERE USUÁRIOS
 def insere_usuario(nome, senha, email):
-    try:
-        with sqlite3.connect('banco.db') as conn:
-            cursor = conn.cursor()
-        
-            # executa query e informa resultado
-            cursor.execute('INSERT INTO usuarios_site(nome, senha, email) VALUES (?, ?, ?)', (nome, senha, email))
-            conn.commit()  
-            print(f"Usuário {nome} inserido com sucesso.")
+    # checa se campos estão todos preenchidos
+    if valida_campos(nome, senha, email):
+        try:
+            with sqlite3.connect('banco.db') as conn:
+                cursor = conn.cursor()
+            
+                # executa query e informa resultado
+                cursor.execute('INSERT INTO usuarios_site(nome, senha, email) VALUES (?, ?, ?)', (nome, senha, email))
+                conn.commit()  
+                print(f"Usuário {nome} inserido com sucesso.")
 
-    except Exception as e:
-        print(f"Ocorreu um erro ao inserir usuário: {e}")
+        except Exception as e:
+            print(f"Ocorreu um erro ao inserir usuário: {e}")
+
+
+# VALIDA CAMPOS
+def valida_campos(nome, senha, email):
+    # Retorna False se os campos estiverem vazios
+    if not nome or not senha or not email:
+        print("Existem campos vazios.")
+        return False
+    else:
+        
+        # Valida email usando a dependência email-validator
+        try:
+            emailinfo = validate_email(email, check_deliverability=False)
+            email = emailinfo.normalized
+            return True
+        except EmailNotValidError as e:
+            print(str(e))
+            return False  
 
 
 # LISTA USUÁRIOS
@@ -58,6 +80,5 @@ def lista_usuarios():
 cria_tabela_usuarios()
 
 insere_usuario("Rodrigo", "123456", "rodukao@gmail.com")
-insere_usuario("Danny", "432432", "dannyzimmermann@yahoo.com.br")
 
 lista_usuarios()
