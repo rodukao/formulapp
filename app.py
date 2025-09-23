b64ece4978ea9cc850aed2a07bfe84b55a0f2144
rodukao <rodukao@gmail.com>
2025-09-23 09:57:42 -0300

    Altera forma de conexão para usar gerenciador de contexto 'with'

--- app.py
+++ app.py
@@ -4,61 +4,48 @@
 import sqlite3
 
 # CRIA TABELA SE NÃO EXISTIR
-conn = sqlite3.connect('banco.db')
-cursor = conn.cursor()
+def cria_tabela_usuarios():
+    with sqlite3.connect('banco.db') as conn:
+        cursor = conn.cursor()
 
-# Criando tabela
-# as 3 aspas são para criar strings de múltiplas linhas facilitando a leitura
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS usuarios_site(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        nome TEXT NOT NULL,
-        senha TEXT NOT NULL,
-        email TEXT NOT NULL UNIQUE
-    )
-''')
-
-# fecha conexão
-conn.close()
+        # Criando tabela
+        # as 3 aspas são para criar strings de múltiplas linhas facilitando a leitura
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS usuarios_site(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nome TEXT NOT NULL,
+                senha TEXT NOT NULL,
+                email TEXT NOT NULL UNIQUE
+            )
+        ''')
 
 # INSERE USUÁRIOS
 def insere_usuario(nome, senha, email):    
-    # conecta no banco e gera o cursor
-    conn = sqlite3.connect('banco.db')
-    cursor = conn.cursor()
+    with sqlite3.connect('banco.db') as conn:
+        cursor = conn.cursor()
     
-    # cria o dicionário usuário
-    usuario = {
-        "nome": nome,
-        "senha": senha,
-        "email": email
-    }
-    
-    # executa e salva a query
-    cursor.execute('''
-        INSERT INTO usuarios_site(nome, senha, email) VALUES (?, ?, ?)
-    ''', (nome, senha, email))
-    conn.commit()
-    
-    print(f"Usuário {nome} inserido com sucesso.")
-    # fecha conexão
-    conn.close()
+        # executa query e informa resultado
+        cursor.execute('INSERT INTO usuarios_site(nome, senha, email) VALUES (?, ?, ?)', (nome, senha, email))
+        conn.commit()  
+        print(f"Usuário {nome} inserido com sucesso.")
+
 
 # LISTA USUÁRIOS
 def lista_usuarios():
-    # conecta no banco e gera cursor
-    conn = sqlite3.connect('banco.db')
-    cursor = conn.cursor()
-    
-    # executa query
-    cursor.execute('SELECT * FROM usuarios_site')
-    resultado = cursor.fetchall()
-    for linha in resultado:
-        print(linha)
+    with sqlite3.connect('banco.db') as conn:
+        cursor = conn.cursor()
         
-    # fecha conexão
-    conn.close()
+        # executa query
+        cursor.execute('SELECT * FROM usuarios_site')
+        resultado = cursor.fetchall()
+        for linha in resultado:
+            print(linha)
+
+
+# CHAMA FUNÇÕES
+cria_tabela_usuarios()
 
 insere_usuario("Rodrigo", "123456", "rodukao@gmail.com")
 insere_usuario("Danny", "432432", "dannyzimmermann@yahoo.com.br")
+
 lista_usuarios()
